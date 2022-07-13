2b08df0c56a094a228b858446eec74882764922a
Audun Halland <audun.halland@pm.me>
2022-07-13 11:58:53 +0200

    chore: Add mockall test, fix CI

---
+++ tests/test_mockall.rs
@@ -0,0 +1,21 @@
+#![cfg(feature = "mockall")]
+use entrait::*;
+
+#[entrait(Foo)]
+fn foo(_deps: &(), arg: i32) -> i32 {
+    arg
+}
+
+fn takes_foo(foo: &impl Foo, arg: i32) -> i32 {
+    foo.foo(arg)
+}
+
+#[test]
+fn test() {
+    let mut mock = MockFoo::new();
+    mock.expect_foo().return_const(42);
+
+    let result = takes_foo(&mock, 1337);
+
+    assert_eq!(42, result);
+}
