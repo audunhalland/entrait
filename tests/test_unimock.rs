a5cc4e8149b2cacf72333d5b747d7ac494809649
Audun Halland <audun.halland@pm.me>
2022-07-14 11:50:32 +0200

    chore: remove nightly-tests feature

--- tests/test_unimock.rs
+++ tests/test_unimock.rs
@@ -280,3 +280,45 @@ mod more_async {
         assert_eq!(42, state.foo().await);
     }
 }
+
+#[cfg(any(feature = "use-async-trait", feature = "use-associated-future"))]
+mod async_no_deps_etc {
+    use entrait::*;
+    use unimock::*;
+
+    struct App;
+
+    #[entrait(Foo, associated_future)]
+    async fn foo(deps: &impl Bar) -> i32 {
+        deps.bar().await
+    }
+
+    #[entrait(Bar, associated_future)]
+    async fn bar(deps: &impl Baz) -> i32 {
+        deps.baz().await
+    }
+
+    #[entrait(Baz, associated_future)]
+    async fn baz(_: &App) -> i32 {
+        42
+    }
+
+    #[entrait(NoDeps, associated_future, no_deps)]
+    async fn no_deps(arg: i32) -> i32 {
+        arg
+    }
+
+    #[tokio::test]
+    async fn test_it() {
+        let app = ::entrait::Impl::new(App);
+        let _ = app.foo().await;
+    }
+
+    #[tokio::test]
+    async fn mock_it() {
+        let unimock = spy([baz::Fn.each_call(matching!()).returns(42).in_any_order()]);
+        let answer = unimock.foo().await;
+
+        assert_eq!(42, answer);
+    }
+}
