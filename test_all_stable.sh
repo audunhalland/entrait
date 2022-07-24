53a3707e96332c3163cc549cc44ae1da5a68e10d
Audun Halland <audun.halland@pm.me>
2022-07-25 01:56:15 +0200

    chore: Update test_all_stable script to run macro unit tests

--- test_all_stable.sh
+++ test_all_stable.sh
@@ -2,4 +2,5 @@
 set -x
 
 cargo hack --feature-powerset --optional-deps "unimock" --exclude-features "default use-associated-future" --exclude-no-default-features test
+cargo test --workspace --features "unimock use-async-trait"
 cargo test --doc --features "unimock use-async-trait"
