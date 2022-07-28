a4c83a2a27f28f03ce1cf2037a8859ce1c8dfc61
Audun Halland <audun.halland@pm.me>
2022-07-28 13:24:59 +0200

    cleanup: Remove unused Parse impl for InputFn

--- entrait_macros/src/input.rs
+++ entrait_macros/src/input.rs
@@ -10,31 +10,12 @@ pub enum Input {
     Trait(syn::ItemTrait),
 }
 
-///
-/// The "body" that is decorated with entrait.
-///
 pub struct InputFn {
     pub fn_attrs: Vec<syn::Attribute>,
     pub fn_vis: syn::Visibility,
     pub fn_sig: syn::Signature,
     // don't try to parse fn_body, just pass through the tokens:
     pub fn_body: proc_macro2::TokenStream,
-}
-
-impl Parse for InputFn {
-    fn parse(input: ParseStream) -> syn::Result<Self> {
-        let fn_attrs = input.call(syn::Attribute::parse_outer)?;
-        let fn_vis = input.parse()?;
-        let fn_sig: syn::Signature = input.parse()?;
-        let fn_body = input.parse()?;
-
-        Ok(InputFn {
-            fn_attrs,
-            fn_vis,
-            fn_sig,
-            fn_body,
-        })
-    }
 }
 
 impl Parse for Input {
